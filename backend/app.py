a4dac0ca0d2e73773971875bda3ae1c96f0e3fba
anik2644 <anik11556@gmail.com>
2024-04-23 11:32:50 +0600

    evenet done

--- backend/app.py
+++ backend/app.py
@@ -61,3 +61,15 @@ async def create_event(event: Event):
         return {"message": "Event created successfully!"}
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Failed to create event: {str(e)}")
+    
+
+@app.get("/get-events/")
+async def get_events():
+    print("in get method!")
+    try:
+        with open("events.txt", "r") as f:
+            events_data = [json.loads(line) for line in f.readlines() if line.strip()]  # Read and parse each line as JSON
+            print("hi i am here!")
+        return {"events": events_data}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Failed to read events: {str(e)}")
