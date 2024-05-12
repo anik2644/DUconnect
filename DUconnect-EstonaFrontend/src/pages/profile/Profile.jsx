220b8ba0d9ebd856e591524246189f68077427e5
anik2644 <anik11556@gmail.com>
2024-05-13 00:36:15 +0600

    done

--- DUconnect-EstonaFrontend/src/pages/profile/Profile.jsx
+++ DUconnect-EstonaFrontend/src/pages/profile/Profile.jsx
@@ -1,22 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCamera, faEdit, faSave, faTimes } from '@fortawesome/free-solid-svg-icons';
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCamera,
+  faEdit,
+  faSave,
+  faTimes,
+} from "@fortawesome/free-solid-svg-icons";
 import "./profile.scss";
 
 const Profile = () => {
   const [image, setImage] = useState(null);
-  const [profilePhoto, setProfilePhoto] = useState('');
+  const [profilePhoto, setProfilePhoto] = useState("");
   const [profileData, setProfileData] = useState(null);
   const [editMode, setEditMode] = useState({});
   const [updateMessage, setUpdateMessage] = useState("");
   const [profileid, setProfileId] = useState("anik11556@gmail.com");
-  const [img_path, setImagepath]= useState("")
+  const [img_path, setImagepath] = useState("");
 
   useEffect(() => {
     fetchProfileData();
   }, []);
-
-
 
   const fetchProfileData = () => {
     console.log(profileid);
@@ -26,22 +29,27 @@ const Profile = () => {
       headers: {
         "Content-Type": "application/json",
       },
-      // body: JSON.stringify(profileid) 
+      // body: JSON.stringify(profileid)
     })
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      console.log('getting data:', data); // Print data to console
-      setProfileData(data); // Set profile data
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log("getting data:", data); // Print data to console
+        // Delete the 'profile_photo' field from the data
+        const { profile_photo, ...profileDataWithoutPhoto } = data;
 
-      console.log('getting profile data:', data['profile_photo']);
-      setProfilePhoto(data['profile_photo'])
-    })
-    .catch(error => console.error('Error fetching profile data:', error));
+        // Set profile data without profile_photo
+        setProfileData(profileDataWithoutPhoto);
+
+        // Set profile photo separately
+        console.log("getting profile data:", profile_photo);
+        setProfilePhoto(profile_photo);
+      })
+      .catch((error) => console.error("Error fetching profile data:", error));
   };
   // const fetchProfileData = () => {
   //   fetch('http://localhost:8888/profile',
@@ -57,17 +65,14 @@ const Profile = () => {
   //     .catch(error => console.error('Error fetching profile data:', error));
   // };
 
-
-
   const handleImageUpload = (event) => {
     const file = event.target.files[0];
     const reader = new FileReader();
 
     if (file) {
-
       const filePath = file.webkitRelativePath || file.name;
       setImagepath(filePath);
-  }
+    }
 
     reader.onloadend = () => {
       setImage(reader.result);
@@ -78,14 +83,14 @@ const Profile = () => {
   };
 
   const toggleEditMode = (key) => {
-    setEditMode(prevState => ({
+    setEditMode((prevState) => ({
       ...prevState,
       [key]: !prevState[key],
     }));
   };
 
   const handleValueChange = (key, newValue) => {
-    setProfileData(prevData => ({
+    setProfileData((prevData) => ({
       ...prevData,
       [key]: newValue,
     }));
@@ -93,15 +98,13 @@ const Profile = () => {
 
   const saveChanges = () => {
     // Here you can implement logic to save changes to the server
-    console.log('Saving changes:', profileData);
+    console.log("Saving changes:", profileData);
     // After saving changes, fetch profile data again
     fetchProfileData();
     setUpdateMessage("Updation Successful");
   };
 
   const updateProfileOnServer = () => {
-
-
     const profileDatabackend = {
       userId: profileData.userId,
       username: profileData.username,
@@ -112,107 +115,119 @@ const Profile = () => {
       session: profileData.session,
       hall: profileData.hall,
       password: profileData.password,
-      profile_photo: img_path
+      // profile_photo: img_path,
+    };
+    if (img_path !== "") {
+      profileDatabackend.profile_photo = img_path;
+    }
 
-  };
+    console.log("profileDatabackend");
+    console.log(profileDatabackend);
 
-  console.log("profileDatabackend");
-  console.log(profileDatabackend);
-
-    fetch('http://localhost:8888/profile', {
-      method: 'POST',
+    fetch("http://localhost:8888/profile", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(profileDatabackend),
     })
-    .then(response => response.json())
-    .then(data => {
-      console.log('Profile updated successfully:', data);
-      setUpdateMessage("Updation Successful");
-      // Reset the update message after 1 second
-  setTimeout(() => {
-    setUpdateMessage("");
-  }, 1000);
-    })
-    .catch(error => {
-      console.error('Error updating profile:', error);
-      setUpdateMessage("Updation Failed");
-      // Reset the update message after 1 second
-  setTimeout(() => {
-    setUpdateMessage("");
-  }, 1000);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Profile updated successfully:", data);
+        setUpdateMessage("Updation Successful");
+        // Reset the update message after 1 second
+        setTimeout(() => {
+          setUpdateMessage("");
+        }, 1000);
+      })
+      .catch((error) => {
+        console.error("Error updating profile:", error);
+        setUpdateMessage("Updation Failed");
+        // Reset the update message after 1 second
+        setTimeout(() => {
+          setUpdateMessage("");
+        }, 1000);
+      });
   };
 
   return (
-    <div>
-      <div style={{ textAlign: 'center', margin: '20px' }}>
-        <div
-          style={{
-            width: '3in',
-            height: '3in',
-            borderRadius: '50%',
-            overflow: 'hidden',
-            margin: '0 auto',
-          }}
-        >
-          {
-          
-          image ? (
-            <img src={image} alt="Profile" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-          ) : 
-          profilePhoto !== '' ? (
-            <img src={profilePhoto} alt="Profile" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
-          ) : (
-            <div
-              style={{
-                width: '100%',
-                height: '100%',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                backgroundColor: '#f0f0f0',
-              }}
-            >
-              <FontAwesomeIcon icon={faCamera} style={{ fontSize: '40px', color: '#555' }} />
-            </div>
-          )}
-        </div>
-        <input type="file" accept="image/*" onChange={handleImageUpload} style={{ marginTop: '10px' }} />
+    <div className="profile-container">
+      <div className="profile-image-container">
+        {image ? (
+          <img src={image} alt="Profile" className="profile-image" />
+        ) : profilePhoto !== "" ? (
+          <img src={profilePhoto} alt="Profile" className="profile-image" />
+        ) : (
+          <div className="camera-icon">
+            <FontAwesomeIcon
+              icon={faCamera}
+              style={{ fontSize: "40px", color: "#555" }}
+            />
+          </div>
+        )}
       </div>
-      <table style={{ margin: '0 auto', borderCollapse: 'collapse', border: '2px solid black' }}>
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        className="upload-input"
+      />
+      <table className="profile-table">
         <tbody>
           {Object.entries(profileData || {}).map(([key, value]) => (
             <tr key={key}>
-              <td style={{ border: '2px solid black', padding: '10px' }}>{key.replace('_', ' ')}</td>
-              <td style={{ border: '2px solid black', padding: '10px' }}>
-                {editMode[key] ? (
+              <td>{key.replace("_", " ")}</td>
+              <td>
+                {key === "password" ? (
+                  editMode[key] ? (
+                    <input
+                      type="password"
+                      value={profileData[key]}
+                      onChange={(e) => handleValueChange(key, e.target.value)}
+                    />
+                  ) : (
+                    "••••••••"
+                  )
+                ) : editMode[key] ? (
                   <input
                     type="text"
-                    value={value}
+                    value={profileData[key]}
                     onChange={(e) => handleValueChange(key, e.target.value)}
                   />
                 ) : (
-                  value
+                  profileData[key]
                 )}
               </td>
-              <td style={{ border: '2px solid black', padding: '10px' }}>
+              <td>
                 {editMode[key] ? (
                   <>
-                    <FontAwesomeIcon icon={faSave} onClick={() => saveChanges()} style={{ cursor: 'pointer', marginRight: '5px' }} />
-                    <FontAwesomeIcon icon={faTimes} onClick={() => toggleEditMode(key)} style={{ cursor: 'pointer' }} />
+                    <FontAwesomeIcon
+                      icon={faSave}
+                      onClick={() => saveChanges()}
+                      className="edit-icons"
+                    />
+                    <FontAwesomeIcon
+                      icon={faTimes}
+                      onClick={() => toggleEditMode(key)}
+                      className="edit-icons"
+                    />
                   </>
                 ) : (
-                  <FontAwesomeIcon icon={faEdit} onClick={() => toggleEditMode(key)} style={{ cursor: 'pointer' }} />
+                  <FontAwesomeIcon
+                    icon={faEdit}
+                    onClick={() => toggleEditMode(key)}
+                    className="edit-icons"
+                  />
                 )}
               </td>
             </tr>
           ))}
         </tbody>
       </table>
-      <button onClick={updateProfileOnServer}>Edit</button>
-      {updateMessage && <p>{updateMessage}</p>}
+      <div className="edit-button">
+        <button onClick={updateProfileOnServer}> Edit Profile </button>
+      </div>
+      {updateMessage && <p className="update-message">{updateMessage}</p>}
     </div>
   );
 };
